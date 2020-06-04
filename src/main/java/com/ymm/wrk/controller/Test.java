5d5e12495cbcda45bd47d0b48195b49dd2d9cb96
wanmin <wanminny@163.com>
2020-06-04 10:25:11 +0800

    mod

--- src/main/java/com/ymm/wrk/controller/Test.java
+++ src/main/java/com/ymm/wrk/controller/Test.java
@@ -10,13 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
         @RequestMapping("/wrk")
         @ResponseBody
-        public int letCpuBusy() {
+        public long busy() {
             int i=0;
             int sum =0;
-            while(i<10000) {
+            long min=1;
+            long max=10000;
+            long num = min + (long)(Math.random() * (max-min+1)) + 500000;
+            while(i<num) {
                 i++;
                 sum+=i;
             }
             return sum;
+        }
+
+        @RequestMapping("/fab")
+        @ResponseBody
+        public long  fab(){
+            long min=1;
+            long max=10000;
+            long num = min + (long)(Math.random() * (max-min+1)) + 10000;
+//            System.out.println(num);
+            long num1 = 1;
+            long num2 = 1;
+            long num3 = 0;
+            for (int i = 1; i < num; i++) {
+                num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3;
+            }
+            return num3;
         }
 }
